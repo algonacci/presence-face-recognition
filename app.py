67013e6311220fd09070e4a583a392a8c991ec53
Eric Julianto <algonacci1@gmail.com>
2023-01-04 21:39:43 +0700

    migration

---
+++ app.py
@@ -0,0 +1,292 @@
+from flask import Flask, render_template, request, redirect, url_for, Response, jsonify
+import mysql.connector
+import cv2
+from PIL import Image
+import numpy as np
+import os
+from datetime import date
+
+app = Flask(__name__)
+
+cnt = 0
+pause_cnt = 0
+justscanned = False
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="flask_db",
+    # charset="utf8mb4"
+)
+
+mycursor = mydb.cursor()
+
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Generate dataset >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+def generate_dataset(nbr):
+    face_classifier = cv2.CascadeClassifier(
+        r"C:\Users\Socio\Desktop\AttendanceHaarCascade\data\haarcascade_frontalface_default.xml")
+
+    def face_cropped(img):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+        # scaling factor=1.3
+        # Minimum neighbor = 5
+
+        if faces == ():
+            return None
+        for (x, y, w, h) in faces:
+            cropped_face = img[y:y + h, x:x + w]
+            return cropped_face
+
+    cap = cv2.VideoCapture(0)
+
+    mycursor.execute("select ifnull(max(img_id), 0) from img_dataset")
+    row = mycursor.fetchone()
+    lastid = row[0]
+
+    img_id = lastid
+    max_imgid = img_id + 100
+    count_img = 0
+
+    while True:
+        ret, img = cap.read()
+        if face_cropped(img) is not None:
+            count_img += 1
+            img_id += 1
+            face = cv2.resize(face_cropped(img), (200, 200))
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+            file_name_path = "dataset/" + nbr + "." + str(img_id) + ".jpg"
+            cv2.imwrite(file_name_path, face)
+            cv2.putText(face, str(count_img), (50, 50),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+            mycursor.execute("""INSERT INTO `img_dataset` (`img_id`, `img_person`) VALUES
+                                ('{}', '{}')""".format(img_id, nbr))
+            mydb.commit()
+
+            frame = cv2.imencode('.jpg', face)[1].tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+            if cv2.waitKey(1) == 13 or int(img_id) == int(max_imgid):
+                break
+                cap.release()
+                cv2.destroyAllWindows()
+
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Train Classifier >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+@app.route('/train_classifier/<nbr>')
+def train_classifier(nbr):
+    dataset_dir = "./dataset"
+
+    path = [os.path.join(dataset_dir, f) for f in os.listdir(dataset_dir)]
+    faces = []
+    ids = []
+
+    for image in path:
+        img = Image.open(image).convert('L')
+        imageNp = np.array(img, 'uint8')
+        id = int(os.path.split(image)[1].split(".")[1])
+
+        faces.append(imageNp)
+        ids.append(id)
+    ids = np.array(ids)
+
+    # Train the classifier and save
+    clf = cv2.face.LBPHFaceRecognizer_create()
+    clf.train(faces, ids)
+    clf.write("classifier.xml")
+
+    return redirect('/')
+
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Face Recognition >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+def face_recognition():  # generate frame by frame from camera
+    def draw_boundary(img, classifier, scaleFactor, minNeighbors, color, clf):
+        gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        features = classifier.detectMultiScale(
+            gray_image, scaleFactor, minNeighbors)
+
+        global justscanned
+        global pause_cnt
+
+        pause_cnt += 1
+        coords = []
+        arr_existing_id = []
+        pred = 100
+        id = 0
+
+        mycursor.execute("select distinct b.img_id "
+                         " from accs_hist a "
+                         " left join img_dataset b on a.accs_prsn = b.img_person "
+                         "where a.accs_date = curdate() ")
+        records = mycursor.fetchall()
+
+        for row in records:
+            arr_existing_id.append(row[0])
+
+        for (x, y, w, h) in features:
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            id, pred = clf.predict(gray_image[y:y + h, x:x + w])
+
+        confidence = int(100 * (1 - pred / 300))
+
+        if id not in arr_existing_id:
+
+            if confidence > 70 and not justscanned:
+
+                mycursor.execute("select a.img_person, b.prs_name, b.prs_role "
+                                 "  from img_dataset a "
+                                 "  left join prs_mstr b on a.img_person = b.prs_nbr "
+                                 " where img_id = " + str(id))
+                row = mycursor.fetchone()
+
+                global pnbr
+                if row is not None:
+                    pnbr = row[0]
+
+                    mycursor.execute("insert into accs_hist (accs_date, accs_prsn) values('" + str(
+                        date.today()) + "', '" + pnbr + "')")
+                    mydb.commit()
+
+            x = 0
+            y = 0
+            w = 0
+            h = 0
+
+            coords = [x, y, w, h]
+        return coords
+
+    def recognize(img, clf, faceCascade):
+        coords = draw_boundary(img, faceCascade, 1.1, 10, (255, 255, 0), clf)
+        return img
+
+    faceCascade = cv2.CascadeClassifier(
+        r"C:\Users\Socio\Desktop\AttendanceHaarCascade\data\haarcascade_frontalface_default.xml")
+    clf = cv2.face.LBPHFaceRecognizer_create()
+    clf.read("classifier.xml")
+
+    # cap = cv2.VideoCapture('http://192.168.43.3:8000')
+    cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture("http://192.168.0.232:4747/video")
+
+    while True:
+        success, img = cap.read()
+        img = recognize(img, clf, faceCascade)
+
+        ret, buffer = cv2.imencode('.jpg', img)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+    cap.release()
+
+
+@app.route('/')
+def home():
+    mycursor.execute(
+        "select prs_nbr, prs_name, prs_role, prs_active, prs_added from prs_mstr")
+    data = mycursor.fetchall()
+
+    return render_template('index.html', data=data)
+
+
+@app.route('/addprsn')
+def addprsn():
+    mycursor.execute("select ifnull(max(prs_nbr) + 1, 101) from prs_mstr")
+    row = mycursor.fetchone()
+    nbr = row[0]
+    # print(int(nbr))
+
+    return render_template('addprsn.html', newnbr=int(nbr))
+
+
+@app.route('/addprsn_submit', methods=['POST'])
+def addprsn_submit():
+    prsnbr = request.form.get('txtnbr')
+    prsname = request.form.get('txtname')
+    prsrole = request.form.get('optrole')
+
+    mycursor.execute("""INSERT INTO `prs_mstr` (`prs_nbr`, `prs_name`, `prs_role`) VALUES
+                    ('{}', '{}', '{}')""".format(prsnbr, prsname, prsrole))
+    mydb.commit()
+
+    # return redirect(url_for('home'))
+    return redirect(url_for('vfdataset_page', prs=prsnbr))
+
+
+@app.route('/vfdataset_page/<prs>')
+def vfdataset_page(prs):
+    return render_template('gendataset.html', prs=prs)
+
+
+@app.route('/vidfeed_dataset/<nbr>')
+def vidfeed_dataset(nbr):
+    # Video streaming route. Put this in the src attribute of an img tag
+    return Response(generate_dataset(nbr), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+    # Return the stream to the client
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/video_feed')
+def video_feed():
+    # Video streaming route. Put this in the src attribute of an img tag
+    return Response(face_recognition(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/fr_page')
+def fr_page():
+    """Video streaming home page."""
+    mycursor.execute("select a.accs_prsn, b.prs_name, b.prs_role, a.accs_added "
+                     "  from accs_hist a "
+                     "  left join prs_mstr b on a.accs_prsn = b.prs_nbr "
+                     " where a.accs_date = curdate() "
+                     " order by 1 desc")
+    data = mycursor.fetchall()
+
+    return render_template('fr_page.html', data=data)
+
+
+@app.route('/countTodayScan')
+def countTodayScan():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        database="flask_db"
+    )
+    mycursor = mydb.cursor()
+
+    mycursor.execute("select count(*) "
+                     "  from accs_hist "
+                     " where accs_date = curdate() ")
+    row = mycursor.fetchone()
+    rowcount = row[0]
+
+    return jsonify({'rowcount': rowcount})
+
+
+@app.route('/loadData', methods=['GET', 'POST'])
+def loadData():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        database="flask_db"
+    )
+    mycursor = mydb.cursor()
+
+    mycursor.execute("select a.accs_prsn, b.prs_name, b.prs_role, date_format(a.accs_added, '%H:%i:%s') "
+                     "  from accs_hist a "
+                     "  left join prs_mstr b on a.accs_prsn = b.prs_nbr "
+                     " where a.accs_date = curdate() "
+                     " order by 1 desc")
+    data = mycursor.fetchall()
+
+    return jsonify(response=data)
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=True)
